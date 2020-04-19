3f7a8c7a8ab8d77f7b121a23d170029534e846b7
laimengling <770669665@qq.com>
2020-04-19 20:25:43 +0800

    'rem设置，自适应布局实现'

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <span class="text">ABCDEF</span>
+        <router-view/>
+    </div>
 </template>
-
-<style>
-
+<script>
+export default {}
+// rem设置，自适应布局实现,设置字体最大1rem=50px
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+<style scoped>
+    .text {
+        font-family: 'Days One';
+        font-size: 1rem;
+        color: orange;
+    }
 </style>
